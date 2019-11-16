c6d1cd43e6469d82f8484eebe32f902005f8aa18
resant18 <santoso.renata@gmail.com>
2019-11-15 18:27:31 -0800

    Add MongoDB connection and remove app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const mongoose = require("mongoose");
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));  
+
 // creates a new Express server
 const express = require("express");
 const app = express();
